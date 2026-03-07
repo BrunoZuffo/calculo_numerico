7bcf6fdcabf9cc22ec52fa449f68e1229c14a39e
Bruno Zuffo <brunoalmeida442@yahoo.com>
2026-03-07 20:10:50 -0300

    start of the def assembly

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import numpy as np
-def Assembly(conec, C):
-    print('teste') 
+
+def Assembly(conec: list[list], C: list[list]):
+    conec = np.array(conec) - 1 # estamos convertendo conec em uma matriz numpy e fazendo decrementações em cada um dos indices para ficar mais pythonic
+    nv = conec.max() + 1 #pegando valor maximo dos nos e somando 1 por conta do indice reduzido anteriormente
+    nc = len(conec) #simplesmente o tanto de linhas na matriz conec
+    A = np.zeros(shape=(nv,nv)) #criando matriz com valores 0 em todas as posições
+    for k in range(nc):
+        n1 = conec[k,0]
+        n2 = conec[k,1]
+        
+
+    return A
