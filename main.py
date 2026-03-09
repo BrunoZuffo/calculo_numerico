8e19a114a98f5c673a12a33c42949c3a383ab282
Victor <vichugo487@gmail.com>
2026-03-09 15:23:27 -0300

    ´main´

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import numpy as np
 
+
+def CalculoCondutancia():
+    pi=np.pi
+    Ak=2.5*10**(-7)
+    mi=0.001
+    Dk=np.sqrt(4*Ak/pi)
+    kk=(pi*Dk**4)/(128*mi)
+    Lk=float(input("Digite o comprimento do cano:"))
+    Ck=kk/Lk #Lk é o comprimento do cano
+
+    print(f"A condutancia é: {Ck}")
+    return Ck
+CalculoCondutancia()
