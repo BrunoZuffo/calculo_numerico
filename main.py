5e4edee33a75a68de7d33f462e59a312fcd8b59d
vbosiplayer <joao.v.bosi@unesp.br>
2026-03-09 15:59:01 -0300

    ´calc_vazao´

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from functions import Assembly, GeraGrafo, PlotaRede, SolveNetwork
+from functions import Assembly, GeraGrafo, PlotaRede, SolveNetwork, calc_vazao
 import numpy as np
 
 # matriz de conectividade do exemplo
@@ -19,6 +19,8 @@ matriz = Assembly(conec, C)
 
 print(matriz)
 
-matriz = SolveNetwork(conec,C,3,1,3)
+pressure = SolveNetwork(conec,C,3,1,3)
 
-print(matriz)
+print(pressure)
+
+matriz_vazao = calc_vazao(conec, C, pressure)
