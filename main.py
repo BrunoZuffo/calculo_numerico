60c96d60ea7220eb8ce37ec6f8735937a198afe9
Bruno Zuffo <brunoalmeida442@yahoo.com>
2026-03-11 11:51:13 -0300

    tkagg

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
+import matplotlib
+matplotlib.use("TkAgg")
+
 from functions import Assembly, GeraGrafo, PlotaRede, SolveNetwork
 import numpy as np
 
 C = np.array([2, 2, 1, 2, 1, 2, 2]) # valores de Ck do exemplo
 
-<<<<<<< HEAD
 
 #gerar grafo
 Xno, conec = GeraGrafo(levels=3)
-=======
-matriz = Assembly(conec, C)
->>>>>>> main
 
 #converter as unidades
 mm_to_m = 0.001
 Xno = Xno * mm_to_m
 
-<<<<<<< HEAD
 #criando condutancia para cada tubo
 C = np.ones(len(conec))
 
@@ -27,8 +25,3 @@ q = calc_vazao(conec + 1, C, pressure)
 
 fig, ax = PlotaRede(conec, Xno, pressure, q, mm_to_m)
 plt.show()
-=======
-matriz = SolveNetwork(conec,C,3,1,3)
-
-print(matriz)
->>>>>>> main
