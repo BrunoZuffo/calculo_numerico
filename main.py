fdba56b778475272a1c7df93fffc7364128f4efb
Victor <vichugo487@gmail.com>
2026-03-11 11:07:40 -0300

    ´main´

--- main.py
+++ main.py
@@ -22,3 +22,24 @@ print(matriz)
 matriz = SolveNetwork(conec,C,3,1,3)
 
 print(matriz)
+
+def SolveNetworkAtualizada(Conec,C,natm,Qs):
+    natm = natm - 1
+
+    Atilde = Assembly(conec, C)
+
+    # condição de pressão atmosférica
+    Atilde[natm, :] = 0
+    Atilde[natm, natm] = 1
+
+    n = Atilde.shape[0]
+
+    b = np.zeros(n)
+
+    # adicionar todas as vazões injetadas
+    for node, q in Qs.items():
+        b[node] = q
+
+    pressure = np.linalg.solve(Atilde, b)
+
+    return pressure
