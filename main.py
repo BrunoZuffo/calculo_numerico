8d3ca92b9f1ea9ffd5a725833732cad9f4f65964
Matheus <baron_matheus@hotmail.com>
2026-03-21 12:10:08 -0300

    Exercício 5 e melhoria na apresentação

--- main.py
+++ main.py
@@ -20,16 +20,27 @@ print(f"Rede gerada com {Xno.shape[0]} nós e {conec.shape[0]} canos.\n")
 
 # EXERCÍCIOS PARA INVESTIGAR O COMPORTAMENTO DO SISTEMA (SEÇÃO 1.4.3)
 
+# ==============================================================================
+# OBSERVAÇÃO SOBRE A INDEXAÇÃO DOS NÓS NAS CONDIÇÕES DE CONTORNO (ps e Qs)
+# ------------------------------------------------------------------------------
+# A função `SolveNetwork` foi originalmente construída com a instrução interna 
+# `i = int(node) - 1`. Isso foi feito para converter numerações amigáveis 
+# (nós de 1 a N) para os índices nativos do Python (que vão de 0 a N-1).
+#
+# Logo, passar o index do nó do dicionário foi feito da seguinte forma:
+# Nó Alvo (n) = Chave Dicionário (n + 1).
+# ==============================================================================
+
 # A: Itens 1 e 2
 
 ps_A = {
-    '1': 100,   # nó 1 → alta pressão (entrada)
-    '4': 0      # nó 4 → baixa pressão (saída)
+    '6': 0.0,       # nó 5 -> pressão atmosférica (saída 1)
+    '216': 0.0      # nó 215 -> pressão atmosférica (saída 2)
 }
 
 Qs_A = {
-    '3': 10,     # nó 3 → injeção de vazão 10
-    '100': 200   # nó 100 → injeção de vazão 200
+    '1': 1.0e-7,    # nó 0 -> injeção de vazão 0.1 mL/s
+    '176': 1.0e-6    # nó 175 -> injeção de vazão 1 mL/s
 }
 
 pressure_A = SolveNetwork(conec, C, ps=ps_A, Qs=Qs_A)
@@ -37,21 +48,62 @@ matriz_vazao_A = calc_vazao(conec, C, pressure_A)
 
 # B: Item 3
 
-ps_B = {
-    '1': 100,
-    str(n_outlet + 1): 0 #"+ 1" por conta da subtração em SolveNetwork
-}
+ps_B = {str(n_outlet + 1): 0}
 
-Qs_B = {} #vazio pois não há injeção de vazão
+# vazão arbitrária de teste (b^(1))
+Q_teste = 1.0e-6
+Qs_B = {'1': Q_teste}
 
-pressure_B = SolveNetwork(conec, C, ps=ps_B, Qs=Qs_B)
-A_original = Assembly(conec, C) #recupera a matriz original (sem ser substituida pelas linhas com 0 e valores de pressão)
-Q_externas = A_original @ pressure_B #calcula o vetor de vazões externas pelo método Ap = Q_ext, agora sabendo o vetor p (pressão recém calculada)
-vazao_inlet = Q_externas[0] #vazão do inlet = vazão do nó 0
+# pressão gerada por essa vazão de teste (p^(1))
+pressure_teste = SolveNetwork(conec, C, ps=ps_B, Qs=Qs_B)
+pressao_no_inlet = pressure_teste[0] # pressão no nó de entrada
 
-print(f"Vazão entrando pelo Inlet (para manter pressão 100): {vazao_inlet:.4e} m³/s\n")
+# escalar alpha para atingir os 100 Pa desejados
+pressao_alvo = 100.0
+alpha = pressao_alvo / pressao_no_inlet
+
+vazao_inlet_real = alpha * Q_teste
+
+print(f"Escalar alpha = {alpha:.2f}):")
+print(f"Vazão necessária para manter 100 Pa no Inlet: {vazao_inlet_real:.4e} m³/s\n")
 
 # C: Itens 4 e 5
+
+t_array = np.linspace(0,10,1000) #discretização do tempo: 1000 passos entre 0 e 10s
+omega_sin = 3.0 #frequência angular
+omega_cos = 4.0
+
+ps_base = {str(n_outlet + 1): 0}
+
+Qs_base_sin = {'1': 1.0e-6} #vazão base do nó de entrada 0 (1 - 1 = 0), 1ml/s
+pressao_base_sin = SolveNetwork(conec, C, ps=ps_base, Qs=Qs_base_sin)
+pressao_maxima_base_sin = np.max(pressao_base_sin)
+
+Qs_base_cos = {'176': 1.0e-6} #vazão base do nó 175 (176 - 1 = 175), 1ml/s
+pressao_base_cos = SolveNetwork(conec, C, ps=ps_base, Qs=Qs_base_cos)
+pressao_maxima_base_cos = np.max(pressao_base_cos)
+
+pressao_maxima_tempo_sin = []
+pressao_maxima_tempo_cos = []
+
+for t in t_array:
+    fator_tempo_sin = 1.0 + 0.1 * np.sin(omega_sin * t)
+    fator_tempo_cos = 1.0 + 0.1 * np.cos(omega_cos * t)
+    
+    p_max_atual_sin = pressao_maxima_base_sin * fator_tempo_sin
+    p_max_atual_cos = pressao_maxima_base_cos * fator_tempo_cos
+    pressao_maxima_tempo_sin.append(p_max_atual_sin)
+    pressao_maxima_tempo_cos.append(p_max_atual_cos)
+
+plt.figure(figsize=(10, 6))
+plt.plot(t_array, pressao_maxima_tempo_sin, color='blue', linewidth=2, label=f'Injeção com sin({omega_sin}t)')
+plt.plot(t_array, pressao_maxima_tempo_cos, color='orange', linewidth=2, label=f'Injeção com cos({omega_cos}t)')
+
+plt.title("Pressão Máxima com Injeção Dinâmica (Itens 4 e 5)")
+plt.xlabel("Tempo (s)")
+plt.ylabel("Pressão Máxima na Rede (Pa)")
+plt.grid(True, linestyle=':', alpha=0.7)
+plt.legend(loc="upper right")
 
 # D: Item 6
 
@@ -77,6 +129,13 @@ for t in t_array:
 
     pressure_t = SolveNetwork(conec, C_t, ps=ps_D, Qs=Qs_D)
     max_pressure.append(np.max(pressure_t))
+
+plt.figure(figsize=(10, 5))
+plt.plot(t_array, max_pressure, color='red', linewidth=2)
+plt.title("Efeito do Aquecimento na Pressão Máxima (Item 6)")
+plt.xlabel("Tempo (s)")
+plt.ylabel("Pressão Máxima na Rede (Pa)")
+plt.grid(True, linestyle='--', alpha=0.7)
 
 # E: Item 7
 
@@ -129,14 +188,6 @@ for level in [1,2,3,4]:
 
 
 # RESULTADOS E PLOTS
-
-#Item 6
-plt.figure(figsize=(10, 5))
-plt.plot(t_array, max_pressure, color='red', linewidth=2)
-plt.title("Efeito do Aquecimento na Pressão Máxima (Item 6)")
-plt.xlabel("Tempo (s)")
-plt.ylabel("Pressão Máxima na Rede (Pa)")
-plt.grid(True, linestyle='--', alpha=0.7)
 
 #Rede Hidráulica
 fig, ax = PlotaRede(conec, Xno, pressure_A, matriz_vazao_A, factor_units=mm_to_m)
